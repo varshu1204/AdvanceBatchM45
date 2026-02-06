6cd9635d2415e7a90623275779505d88c82bc359
QSP <QSP@DESKTOP-K5OA249>
2026-02-06 07:22:25 +0530

    First time committed

---
+++ src/test/java/Practice/Demo1Test.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	
+	@Test(groups = "smoke")
+	public void fruitsTest()
+	{
+		System.out.println("apple");
+	}
+	
+	
+}
---
+++ src/test/java/Practice/DemoTest.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	
+	@Test(groups = "regression")
+	public void colourTest()
+	{
+		System.out.println("Black");
+	}
+}
